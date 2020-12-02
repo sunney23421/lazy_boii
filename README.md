458a1b556aa875ba1a153457a6a1139e925fa414
Chaiwat Kasetsinchai <Doglnw8@gmail.com>
2020-12-02 12:52:21 +0700

    frist upload

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # lazy_boii
+
 Python project
+
+I'm realy lazy to do a stupit work. So I just worte python script.
+
+I have choice to spent 2 days to do give all question or whole week just write scrpit....
+Yep.. modern problems require modern solutions.:joy_cat::joy_cat:
+
+
+<img src="https://media.giphy.com/media/LzKhSiHnpMyHu/giphy.gif"  />
+
+I've been assigned to give all google from qusetion to 200 students. I though I'm not gonna do that :shit: so I decided to write a python script to help me to done my task for me.
+
+I used PyCliker to clike to all google from which is kinna hard code but it's much more flexible than others methon.
+
+This not only just ramdon cliker but I put effort to implement some login depent on the question. Which is you can change all number as you wish. :ok_hand:
---
+++ real.py
@@ -0,0 +1,117 @@
+from pynput.mouse import Button, Controller
+from pynput.keyboard import Listener, KeyCode
+import time
+import random
+
+mouse = Controller()
+#mouse.position=(630, 814)
+i=1;
+
+def stronglyAgreee():
+  randomThrToFive = random.randint(1,11);
+  #randomThrToFive = 9;
+  if (randomThrToFive==1):
+    mouse.position=(626, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (1)
+  elif randomThrToFive==2:
+    mouse.position=(699, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (2)
+  elif randomThrToFive>=3 and randomThrToFive<=5:
+    mouse.position=(771, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (3)
+  elif randomThrToFive>=6 and randomThrToFive<=8:
+    mouse.position=(843, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (4)
+  else:
+    mouse.position=(917,822);time.sleep(0.25);mouse.click(Button.left,1)#;print (5)
+  #print (randomThrToFive)
+  time.sleep(0.5);
+  mouse.scroll(0,-2.5)
+  randomThrToFive = random.randint(1,11);
+  
+def stronglyDisagree():
+  randomThrToFive = random.randint(1,11);
+  if (randomThrToFive==1):
+    mouse.position=(917,822);time.sleep(0.25);mouse.click(Button.left,1)#;print (5)
+  elif randomThrToFive==2:
+    mouse.position=(843, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (4)
+  elif randomThrToFive>=3 and randomThrToFive<=5:
+    mouse.position=(771, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (3)
+  elif randomThrToFive>=6 and randomThrToFive<=8:
+    mouse.position=(699, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (2)
+  else:
+    mouse.position=(626, 822);time.sleep(0.25);mouse.click(Button.left,1)#;print (1)
+  #print (randomThrToFive)
+  time.sleep(0.5);
+  mouse.scroll(0,-2.5)
+  randomThrToFive = random.randint(1,11);
+
+def stronglyAlwasys():
+  randomThrToFive = random.randint(1,11);
+  #randomThrToFive = 9;
+  if (randomThrToFive==1):
+    mouse.position=(565, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (1)
+  elif randomThrToFive==2:
+    mouse.position=(658, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (2)
+  elif randomThrToFive>=3 and randomThrToFive<=5:
+    mouse.position=(752, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (3)
+  elif randomThrToFive>=6 and randomThrToFive<=8:
+    mouse.position=(848, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (4)
+  else:
+    mouse.position=(944,747);time.sleep(0.25);mouse.click(Button.left,1)#;print (5)
+  #print (randomThrToFive)
+  time.sleep(0.5);
+  mouse.scroll(0,-2.5)
+  randomThrToFive = random.randint(1,11);
+  
+def stronglyNever():
+  randomThrToFive = random.randint(1,11);
+  if (randomThrToFive==1):
+    mouse.position=(944,747);time.sleep(0.25);mouse.click(Button.left,1)#;print (5)
+  elif randomThrToFive==2:
+    mouse.position=(848, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (4)
+  elif randomThrToFive>=3 and randomThrToFive<=5:
+    mouse.position=(752, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (3)
+  elif randomThrToFive>=6 and randomThrToFive<=8:
+    mouse.position=(658, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (2)
+  else:
+    mouse.position=(565, 747);time.sleep(0.25);mouse.click(Button.left,1)#;print (1)
+  #print (randomThrToFive)
+  time.sleep(0.5);
+  mouse.scroll(0,-2.5)
+  randomThrToFive = random.randint(1,11);
+#while i<20:
+    #stronglyDisagree()
+    
+    #mouse.scroll(0,2.5)
+    #i=i+1
+
+stronglyAgreee()
+stronglyAgreee()
+stronglyDisagree()
+stronglyDisagree()
+stronglyAgreee()
+stronglyAgreee()
+stronglyAgreee()
+stronglyAgreee()
+stronglyAgreee()
+stronglyAgreee();mouse.scroll(0,-2.58)#1######10
+
+while i<16:
+    stronglyNever()
+    i=i+1
+
+mouse.scroll(0,-2)
+i=1;
+while i<20:
+    stronglyNever()
+    i=i+1
+
+mouse.scroll(0,-2.5)
+i=1;
+while i<15:
+    stronglyAgreee()
+    i=i+1
+mouse.scroll(0,-2.5)
+mouse.scroll(0,-2.5)
+i=1;
+while i<29:
+    stronglyAgreee()
+    i=i+1
+  
